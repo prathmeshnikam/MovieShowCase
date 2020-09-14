85b989b9a4e9e0c7b7414ffd46c64c3a357b85eb
prathmesh.nikam <prathmesh.nikam@udio.in>
2020-09-14 10:55:49 +0530

    Add all moview files

---
+++ .history/script/movies_20200913183553.js
@@ -0,0 +1,90 @@
+ 
+var movieList = [];
+var allMovieList = [];
+
+// Call _getMovieList Function in onload
+ _getMovieList = () => { 
+	var request = new XMLHttpRequest()
+	request.open(_service_apiRequrstType.GET, _service_apiUrl.getMoviesList, true)
+
+	request.onload = function () {
+		if (request.status >= 200 && request.status < 400) {
+			var finalData = JSON.parse(this.response);
+			if (finalData.code == 0) {
+				movieList = finalData.data.movieList;
+				allMovieList = finalData.data.movieList;
+				_service_addMultipleInService(movieList);
+			} else {
+				_service_showErrorMsg('Somthing Went Wrong!');
+			}
+		} else {
+			_service_showErrorMsg('Unable to call api');
+		}
+	}
+
+	// Send a request
+	request.send()
+}
+
+// Delete Movie using Id
+_deleteMovie = (id) => {
+	var index = allMovieList.findIndex(function (o) {
+		return o.id === id;
+	})
+	if (index !== -1) allMovieList.splice(index, 1);
+ 
+	_service_deleteElements();  
+	movieList = allMovieList;
+	_service_addMultipleInService(movieList); 
+}
+
+// Serch movie based on user type in serch box
+_serchMovie = () => {
+	var filterValue = document.getElementById("movieTitle").value;
+	_service_deleteElements(); 
+	movieList = allMovieList.filter(option => option.title.toLowerCase().startsWith(filterValue)); 
+	_service_addMultipleInService(movieList);
+}
+
+ _addNewElements = () => {
+	const container = document.createElement('div')
+	container.setAttribute('class', 'container')
+	app.appendChild(container)
+}
+
+// This will open poup when user click on Add movie Button 
+_openPopup = () => {
+	document.getElementById('addNewMovie').style.display = 'block';
+}
+
+//Call on add new movie button which is in popup
+_addNewMovie = () => { 
+	let title = document.getElementById("title").value;
+	let year = document.getElementById("year").value;
+	let posterUrl = document.getElementById("posterUrl").value;
+	if (title && year && posterUrl) {
+		let newData = {
+			title: title,
+			year: year,
+			posterUrl: posterUrl,
+			id: _service_generateUniqueId()
+		}
+		_service_addViewInDomInService(newData); 
+		allMovieList.push(newData);
+		
+		document.getElementById('addNewMovie').style.display = 'none';
+		document.getElementById('congratsMsg').style.display = 'block';
+		document.getElementById("title").value = ''; 
+		document.getElementById("year").value = '';
+		document.getElementById("posterUrl").value = '';
+	} else {
+		alert("Please fill all fileds")
+	} 
+}
+
+//Close poup based on popup name
+_closeModel = (popup_name) => {
+	document.getElementById(popup_name).style.display = 'none';
+}
+ 
+ 
---
+++ .history/script/service_20200913182059.js
@@ -0,0 +1,100 @@
+ 
+ window._service_apiUrl = {
+    getMoviesList: "https://run.mocky.io/v3/eda3bc72-1f38-462c-82d8-df0a3045d861"
+}
+
+window._service_apiRequrstType = {
+    GET: "GET",
+    POST: "POST",
+    DELETE: "DELETE"
+}
+
+window._service_addMultipleInService = function _service_addMultipleInService(allData){
+    const app = document.getElementById('root')
+	const container = document.createElement('div')
+	container.setAttribute('class', 'container')
+	app.appendChild(container)
+
+	allData.forEach((movie) => { 
+		const card = document.createElement('div')
+		card.setAttribute('class', 'card')
+
+		const h3 = document.createElement('h3')
+		h3.textContent = movie.title
+
+		const h4 = document.createElement('h4')
+		h4.textContent = movie.year
+
+		const logo = document.createElement('img')
+		logo.src = movie.posterUrl
+
+		var deleteButton = document.createElement("input");
+		deleteButton.type = 'button';
+		deleteButton.value = 'Delete';
+		deleteButton.id = movie.id;
+		deleteButton.className = 'button';
+		deleteButton.onclick = function () {
+			_deleteMovie(movie.id);
+		};
+
+		container.appendChild(card)
+		card.appendChild(logo)
+		card.appendChild(h3)
+		card.appendChild(h4)
+		card.appendChild(deleteButton)
+	})
+}
+
+window._service_addViewInDomInService = function _service_addMultipleInService(data){
+    const app = document.getElementById('root')
+    const container = document.createElement('div')
+    container.setAttribute('class', 'container')
+    app.appendChild(container)
+
+    const card = document.createElement('div')
+    card.setAttribute('class', 'card')
+
+    const h3 = document.createElement('h3')
+    h3.textContent = data.title
+
+    const h4 = document.createElement('h4')
+    h4.textContent = data.year
+
+    const logo = document.createElement('img')
+    logo.src = data.posterUrl
+
+    var deleteButton = document.createElement("input");
+    deleteButton.type = 'button';
+    deleteButton.value = 'Delete';
+    deleteButton.id = data.id;
+    deleteButton.className = 'button';
+    deleteButton.onclick = function () {
+        _deleteMovie(data.id);
+    };
+    container.appendChild(card)
+    card.appendChild(logo)
+    card.appendChild(h3)
+    card.appendChild(h4)
+    card.appendChild(deleteButton)
+}
+
+window._service_deleteElements = function _service_deleteElements(){
+	var element = document.getElementsByClassName("container"),
+		index;
+	for (index = element.length - 1; index >= 0; index--) {
+		element[index].parentNode.removeChild(element[index]);
+	}
+}
+
+window._generateUniqueId = function _generateUniqueId(){
+	let stringFourChar = () => {
+		return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+	}
+	return stringFourChar() + '-' + stringFourChar() + '-' + stringFourChar() + stringFourChar();
+}
+
+window._showErrorMsg = function _showErrorMsg(msg){
+    const errorMessage = document.createElement('marquee')
+	errorMessage.textContent = msg;
+	app.appendChild(errorMessage)
+}
